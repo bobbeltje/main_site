1998cc2663c39fade0807164d5db42bf5558bab7
Matthias <matthiaswubs@gmail.com>
2019-09-26 22:14:34 +0200

    error pages added

--- main_site/routes.py
+++ main_site/routes.py
@@ -51,3 +51,15 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('home'))
+
+@app.errorhandler(404)
+def error_404(error):
+    return render_template('errors/404.html'), 404
+
+@app.errorhandler(403)
+def error_403(error):
+    return render_template('errors/403.html'), 403
+
+@app.errorhandler(500)
+def error_500(error):
+    return render_template('errors/500.html'), 500
